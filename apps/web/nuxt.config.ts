802666d73f251549fb3d38483d702c696904e126
Sebastian Stepper <Sebastian-Stepper@gmx.de>
2026-04-28 20:17:04 +0200

    init web and api

---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  eslint: {
+    config: {
+      stylistic: false,
+    },
+  },
+});
