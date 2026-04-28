6f37075abfe99f9c06bc6dc0e6185d28ea3f1461
Sebastian Stepper <Sebastian-Stepper@gmx.de>
2026-04-28 21:18:22 +0200

    docker setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(
+    process.env.API_PORT ?? 3000,
+    process.env.API_HOST ?? '0.0.0.0',
+  );
 }
 void bootstrap();
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  runtimeConfig: {
+    apiBase: process.env.WEB_API_BASE,
+  },
   eslint: {
     config: {
       stylistic: false,
