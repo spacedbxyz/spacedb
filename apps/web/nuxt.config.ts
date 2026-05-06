95c886ca177db65757685eea8920680dd2475fe6
Sebastian Stepper <Sebastian-Stepper@gmx.de>
2026-05-06 20:50:40 +0200

    feat: init nuxt/ui

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   devServer: {
     host: process.env.WEB_HOST || '0.0.0.0',
     port: Number(process.env.WEB_PORT) || 3001,
